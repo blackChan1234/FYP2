56a4c1dd127725d9746dc2dc3884f6c2a7e4afe8
s7e7a <126228000+s7e7a@users.noreply.github.com>
2024-02-02 11:48:28 +0800

    下滑

--- app/src/main/java/com/example/fyp/foodrecommend.java
+++ app/src/main/java/com/example/fyp/foodrecommend.java
@@ -16,6 +16,8 @@ public class foodrecommend extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.foodrecommend);
+
+        //example data
         nutritionFactsList.add(new NutritionFact("Calories", "484 kcal"));
         ingredientsList.add(new Ingredient("Salmon", "a001", this)); // Notice 'this' is passed for context
 
