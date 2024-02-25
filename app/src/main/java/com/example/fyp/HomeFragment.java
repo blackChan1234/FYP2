3a9d66f8ce8385f5f225379cf97f56e089bbdc67
Nam-1234 <142138453+Nam-1234@users.noreply.github.com>
2024-02-25 23:04:17 +0800

    setting

--- app/src/main/java/com/example/fyp/HomeFragment.java
+++ app/src/main/java/com/example/fyp/HomeFragment.java
@@ -1,10 +1,12 @@
 package com.example.fyp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
 
@@ -17,5 +19,20 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btnSetting = view.findViewById(R.id.btnSetting);
+        btnSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Make sure to use getActivity() to get the context
+                Intent intent = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
