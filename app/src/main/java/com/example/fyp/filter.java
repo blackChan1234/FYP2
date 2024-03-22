ed41e2e4dce29a7429bda8cedcffee27d541e9d0
Mars-Chau <138330211+Mars-Chau@users.noreply.github.com>
2024-03-22 22:58:17 +0800

    update userInfo and search

--- app/src/main/java/com/example/fyp/filter.java
+++ app/src/main/java/com/example/fyp/filter.java
@@ -22,52 +22,13 @@ public class filter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.findrestaurants);
         mContext = filter.this;
-        ImageButton btn_filter = findViewById(R.id.btn_filter);
-        btn_filter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                initPopWindow(v);
-            }
-        });
+
         ImageButton btn_arrow_back = findViewById(R.id.arrow_back);
         btn_arrow_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(filter.this, MainActivity.class);
                 startActivity(intent);
-            }
-        });
-    }
-
-    private void initPopWindow(View v) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.restaurants_filter, null, false);
-        Button btn_xixi = (Button) view.findViewById(R.id.btn_xixi);
-        Button btn_hehe = (Button) view.findViewById(R.id.btn_hehe);
-        final PopupWindow popWindow = new PopupWindow(view,
-                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
-
-        popWindow.setTouchable(true);
-        popWindow.setTouchInterceptor(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return false;
-            }
-        });
-        popWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
-
-        popWindow.showAsDropDown(v, 50, 0);
-
-        btn_xixi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(filter.this, "你点击了嘻嘻~", Toast.LENGTH_SHORT).show();
-            }
-        });
-        btn_hehe.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(filter.this, "你点击了呵呵~", Toast.LENGTH_SHORT).show();
-                popWindow.dismiss();
             }
         });
     }
