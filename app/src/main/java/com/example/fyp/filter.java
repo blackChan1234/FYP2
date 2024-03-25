e89fd449f184432e65bd4810f74b473ffc941359
Mars-Chau <138330211+Mars-Chau@users.noreply.github.com>
2024-03-25 23:03:06 +0800

    update search ui

--- app/src/main/java/com/example/fyp/filter.java
+++ app/src/main/java/com/example/fyp/filter.java
@@ -14,17 +14,20 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 public class filter extends AppCompatActivity {
-    private Context mContext;
     private LinearLayout dropdownLayout;
     private ImageButton filter_hide,area_hide,preference_hide,cuisine_hide;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.findrestaurants);
-        mContext = filter.this;
+
+        searchView = findViewById(R.id.search_view);
+        searchView.setSubmitButtonEnabled(true);
 
         ImageButton btn_arrow_back = findViewById(R.id.arrow_back);
         btn_arrow_back.setOnClickListener(new View.OnClickListener() {
@@ -40,13 +43,7 @@ public class filter extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dropdownLayout = findViewById(R.id.filter);
-                if (dropdownLayout.getVisibility() == View.VISIBLE) {
-                    dropdownLayout.setVisibility(View.GONE);
-                    filter_hide.setImageResource(R.drawable.baseline_arrow_drop_down_24);
-                } else {
-                    dropdownLayout.setVisibility(View.VISIBLE);
-                    filter_hide.setImageResource(R.drawable.baseline_arrow_drop_up_24);
-                }
+                hideFilter(filter_hide, dropdownLayout);
             }
         });
 
@@ -55,13 +52,7 @@ public class filter extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dropdownLayout = findViewById(R.id.area);
-                if (dropdownLayout.getVisibility() == View.VISIBLE) {
-                    dropdownLayout.setVisibility(View.GONE);
-                    area_hide.setImageResource(R.drawable.baseline_arrow_drop_down_24);
-                } else {
-                    dropdownLayout.setVisibility(View.VISIBLE);
-                    area_hide.setImageResource(R.drawable.baseline_arrow_drop_up_24);
-                }
+                hideFilter(area_hide, dropdownLayout);
             }
         });
 
@@ -70,30 +61,28 @@ public class filter extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dropdownLayout = findViewById(R.id.preference);
-                if (dropdownLayout.getVisibility() == View.VISIBLE) {
-                    dropdownLayout.setVisibility(View.GONE);
-                    preference_hide.setImageResource(R.drawable.baseline_arrow_drop_down_24);
-                } else {
-                    dropdownLayout.setVisibility(View.VISIBLE);
-                    preference_hide.setImageResource(R.drawable.baseline_arrow_drop_up_24);
-                }
+                hideFilter(preference_hide, dropdownLayout);
             }
         });
 
-        cuisine_hide = findViewById(R.id.area_hide);
+        cuisine_hide = findViewById(R.id.cuisine_hide);
         cuisine_hide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dropdownLayout = findViewById(R.id.cuisine);
-                if (dropdownLayout.getVisibility() == View.VISIBLE) {
-                    dropdownLayout.setVisibility(View.GONE);
-                    cuisine_hide.setImageResource(R.drawable.baseline_arrow_drop_down_24);
-                } else {
-                    dropdownLayout.setVisibility(View.VISIBLE);
-                    cuisine_hide.setImageResource(R.drawable.baseline_arrow_drop_up_24);
-                }
+                hideFilter(cuisine_hide, dropdownLayout);
             }
         });
+    }
+
+    public void hideFilter(ImageButton btn, LinearLayout dropdownLayout) {
+        if (dropdownLayout.getVisibility() == View.VISIBLE) {
+            dropdownLayout.setVisibility(View.GONE);
+            btn.setImageResource(R.drawable.baseline_arrow_drop_down_24);
+        } else {
+            dropdownLayout.setVisibility(View.VISIBLE);
+            btn.setImageResource(R.drawable.baseline_arrow_drop_up_24);
+        }
     }
 }
 
