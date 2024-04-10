04ed65efbb7bc23bb15cfde7300df3ed32656892
s7e7a <126228000+s7e7a@users.noreply.github.com>
2024-04-10 22:35:21 +0800

    glogin

--- app/src/main/java/com/example/fyp/LoginActivity.java
+++ app/src/main/java/com/example/fyp/LoginActivity.java
@@ -1,16 +1,26 @@
 package com.example.fyp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.developer.gbuttons.GoogleSignInButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,7 +38,7 @@ public class LoginActivity extends AppCompatActivity {
         usernameEditText = findViewById(R.id.email);
         passwordEditText = findViewById(R.id.password);
         loginButton = findViewById(R.id.login);
-        gbtn = findViewById(R.id.gbtn)
+        gbtn = findViewById(R.id.gbtn);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,7 +47,44 @@ public class LoginActivity extends AppCompatActivity {
         });
         gOptions= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         gClient= GoogleSignIn.getClient(this,gOptions);
-        
+
+        GoogleSignInAccount gAccount =GoogleSignIn.getLastSignedInAccount(this);
+
+
+        if (gAccount != null) {
+            finish();
+            Intent intent = new Intent( LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+        new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    Task<GoogleSignInAccount> task =GoogleSignIn.getSignedInAccountFromIntent(data);
+                    try {
+                        task.getResult(ApiException.class);
+                        finish();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                    }catch (ApiException e){
+                        Toast.makeText(LoginActivity.this,"ApiException",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        gbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = gClient.getSignInIntent();
+                activityResultLauncher.launch(signInIntent);
+            }
+
+
+
+        });
+
     }
 
     private void performLogin() {
